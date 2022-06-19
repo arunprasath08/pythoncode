6310af82f4cf17fd19c9a41bc63ce0dbad70f4e3
arunprasath08 <arun.prasath509@gmail.com>
2022-06-19 18:20:05 +0530

    math.pow() and pandas
    math library and pandas library learning

---
+++ math_pow_func.py
@@ -0,0 +1,9 @@
+import math
+
+
+num=2
+print(num,int(math.pow(num,2)))
+print(num,int(math.pow(num,1.0)))
+print(num,int(math.pow(num,1.0)))
+print(num,math.pow(1.0,num))
+print(num,math.pow(num,0.0))
---
+++ pandas_learning.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+
+tabA={'Pkey':[1,2,np.nan,1],
+'Cval':['a','b','c','d']}
+tabB={'Pkey':[1,2,3,np.nan],
+'Cval':['a','b','c','e']}
+
+tabA_df=pd.DataFrame(data=tabA)
+tabB_df=pd.DataFrame(data=tabB)
+
+# print(tabA_df.head())
+# print(tabB_df.head())
+
+tabJoin_df=tabA_df.merge(tabB_df,how='cross')
+
+print(tabJoin_df)
