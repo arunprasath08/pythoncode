2604738661d88732811bf2506361ae2855f4bfce
arunprasath08 <arun.prasath509@gmail.com>
2025-10-01 23:52:18 +0530

    list operations file added

--- List_operations.py
+++ List_operations.py
@@ -2,16 +2,16 @@
 #lst = [12, 5, 8, 19, 3]
 
 #list declaration
-lst=list()
-# lst=[]
+#lst=list()
+lst=[]
 
 #Getting values from user and appending to list
-for x in range(0,6):
-    lst.append(int(input("Enter value : ")))
+# for x in range(0,6):
+#     lst.append(int(input("Enter value : ")))
 #lst.append(x for x in range (0,3))
 
 # list comprehension
-# lst=[int(input(f'Enter values for list element {i+1} : ')) for i in range(6)]
+lst=[int(input(f'Enter values for list element {i+1} : ')) for i in range(6)]
 
 print('Provided list is : ', lst)
 
